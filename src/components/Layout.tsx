cd6ce8e0543779a76bb7043e46389e8486f28f7a
SantiagoRomero00 <saantiagoromero2000@gmail.com>
2026-01-04 13:17:52 -0300

    Updated index.css

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,12 +3,12 @@ import Header from './Header';
 
 const Layout = () => {
   return (
-    <>
+    <div className="overflow-x-hidden w-full relative">
       <Header />
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
 
